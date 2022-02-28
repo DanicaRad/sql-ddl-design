e0b8857304a51af10dc061485b325e6e57790968
DanicaRad <87402525+DanicaRad@users.noreply.github.com>
2022-02-27 23:05:33 -0800

    commented out old schema

--- music.sql
+++ music.sql
@@ -31,3 +31,40 @@ VALUES
   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
   ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
   ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+
+DROP DATABASE IF EXISTS music;
+
+CREATE DATABASE music;
+
+\c music
+
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  album_name TEXT NOT NULL,
+  artist_id INT REFERENCES artists
+);
+
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE songs
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  duration_in_seconds INT NOT NULL,
+  release_date DATE NOT NULL,
+  artists INT[] NOT NULL REFERENCES artists,
+  album INT NOT NULL REFERENCES albums,
+  producers INT[] NOT NULL REFERENCES producers
+);
